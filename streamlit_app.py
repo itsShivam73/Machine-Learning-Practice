ef262824fba8ea1d0167b2b765908be6585557bd
itsShivam73 <shivampandey936978@gmail.com>
2025-12-31 13:08:11 +0530

    Refactor inputs to use selectbox and slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,37 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+# Load model
 model = pickle.load(open("Linear_RegressionModel.pkl", "rb"))
 
 st.title("🚗 Car Price Prediction App")
 
-name = st.text_input("Car Name", "Maruti Suzuki Swift")
-company = st.text_input("Company", "Maruti")
-year = st.number_input("Year", 2000, 2025, 2019)
-kms = st.number_input("KMs Driven", 0, 300000, 120)
-fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "CNG"])
+# Dropdown options (you can expand later)
+car_names = [
+    "Maruti Suzuki Swift",
+    "Maruti Suzuki Alto",
+    "Hyundai i20",
+    "Hyundai Creta",
+    "Honda City",
+    "Toyota Innova"
+]
 
+companies = ["Hyundai", "Mahindra", "Ford", "Maruti", "Skoda", "Audi", "Toyota",
+ "Renault", "Honda", "Datsun", "Mitsubishi", "Tata", "Volkswagen",
+ "Chevrolet", "Mini", "BMW", "Nissan", "Hindustan", "Fiat", "Force",
+ "Mercedes", "Land", "Jaguar", "Jeep", "Volvo"]
+
+
+fuel_types = ["Petrol", "Diesel", "CNG"]
+
+# Inputs
+name = st.selectbox("Car Name", car_names)
+company = st.selectbox("Company", companies)
+year = st.selectbox("Year", list(range(2000, 2026))[::-1])
+kms = st.slider("KMs Driven", 0, 300000, 120, step=1000)
+fuel = st.selectbox("Fuel Type", fuel_types)
+
+# Prediction
 if st.button("Predict Price"):
     df = pd.DataFrame([[name, company, year, kms, fuel]],
         columns=['name','company','year','kms_driven','fuel_type'])
